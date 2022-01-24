c921556615c8edc64b9789910b2ee08b5f1488c3
yourself101010 <91150755+yourself101010@users.noreply.github.com>
2022-01-24 18:57:40 -0500

    myInfo version 2

--- myInfo.py
+++ myInfo.py
@@ -1,5 +1,8 @@
-#CS175-50
+#CS175L-50
 #Gil Eckert
 #myInfo
 print("My name is Tennessee Tremain")
-print("I am a student")
+#print("I am a student")
+print("I live at 123 Grilli St, Marlton, NJ, 07764")
+print("My phone number is 555-123-4567")
+print("My college major is Computer Science")
