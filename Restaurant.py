1864bd79589523af508623cfa30fa6a44cd2eb06
yourself101010 <91150755+yourself101010@users.noreply.github.com>
2022-02-07 19:01:08 -0500

    Restaurant Program

---
+++ Restaurant.py
@@ -0,0 +1,33 @@
+"""
+Name: Tennessee Tremain
+Program: Restaurant
+"""
+
+vegetarian = False
+vegan = False
+gluten = False
+
+inp = input("Is anyone in your party a vegetarian?\n")
+if inp.lower() == "yes":
+    vegetarian = True
+
+inp = input("Is anyone in your party a vegan?\n")
+if inp.lower() == "yes":
+    vegan = True
+
+inp = input("Is anyone in your party gluten-free?\n")
+if inp.lower() == "yes":
+    gluten = True
+
+if vegan:
+    print("Here are your restaurant choices:" +
+    "\nCorner Café\nThe Chef's Kitchen")
+elif gluten:
+    print("Here are your restaurant choices:" +
+    "\nMain Street Pizza Company\nCorner Café\nThe Chef's Kitchen")
+elif vegetarian:
+    print("Here are your restaurant choices:" +
+        "\nMain Street Pizza Company\nCorner Café\nMama's Fine Italian\nThe Chef's Kitchen")
+else:
+    print("Here are your restaurant choices:" +
+        "\nJoe's Gourmet Burgers\nMain Street Pizza Company\nCorner Café\nMama's Fine Italian\nThe Chef's Kitchen")
